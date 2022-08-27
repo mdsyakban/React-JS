90b6fab527df18986f241f5206d3ed7c5d00226b
Ahmad Syakban <ahmadsyakban240@gmail.com>
2022-08-27 19:17:37 +0700

    Edit About

--- src/componen/about/About.jsx
+++ src/componen/about/About.jsx
@@ -12,24 +12,24 @@ function About() {
       <div className='col-12'>
       <div className='row'>
 
-        <div className='col-12 col-sm-6 col-md-6 col-lg-6 py-3 bg-secondary'>
+        <div className='col-12 col-sm-6 col-md-6 col-lg-6 py-3 about'>
         <div className="text-center">
           <img src={syakban} className='about-img' alt='syakban' />
         </div>
         </div>
 
-        <div className='col-12 col-sm-6 col-md-6 col-lg-6 text bg-primary'>
+        <div className='col-12 col-sm-6 col-md-6 col-lg-6 text about'>
         <h3>About Me</h3>
           <p>Hallo Salam kenal, Nama saya Ahmad Syakban. Saya berasal dari Desa Lenangguar Kecamatan Lenangguar Kabupaten Sumbawa Provinsi Nusa Tenggara Barat. Saat ini saya tengah mengenyam pendidikan di salah satu kampus di pulau Sumbawa, yaitu Universitas Teknologi
              Sumbawa Fakultas Rekayasa Sistem Prodi Teknik Informatika.</p>
         </div>
 
-        <div className='col-12 col-sm-6 col-md-6 col-lg-6 text bg-danger'>
+        <div className='col-12 col-sm-6 col-md-6 col-lg-6 text about'>
         <h3>Alamat :</h3>
           <p>Dusun Lenangguar B RT 011 / RW 004 Desa Lenangguar Kecamatan Lenangguar Kabupaten Sumbawa Provinsi Nusa Tenggara Barat.</p>
         </div>
 
-        <div className='col-12 col-sm-6 col-md-6 col-lg-6 text bg-success'>
+        <div className='col-12 col-sm-6 col-md-6 col-lg-6 text about'>
         <h4>Riwayat Pendidikan :</h4>
         <li>SDN 1 Lenangguar 2005-2011</li>
         <li>SMPN 1 Lenangguar 2011-2014</li>
